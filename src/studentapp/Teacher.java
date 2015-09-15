67df33a86e742e697d5bb476e4fb15410390d662
mowyas <mowyas@10.0.1.61>
2015-09-15 17:35:16 +0200

    Initial Commit, Everything for the prac.

---
+++ src/studentapp/Teacher.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package studentapp;
+
+/**
+ *
+ * @author mowyas
+ */
+public class Teacher {
+    private String name = "Unallocated";
+    private String groupName = "00UA";
+    private String phoneNumber = "0000000000";
+    
+    public Teacher() {
+    }
+    
+    
+}
